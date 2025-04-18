c957b5de966fe19343aca9c1f910cd978ad4115b
dhrubojyoti <lnsaha1214@gmail.com>
2025-04-19 00:48:25 +0600

    Step 26

--- Meow.cpp
+++ Meow.cpp
@@ -1,3 +1,4 @@
+//#include "Meow.h"
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,23 +6,34 @@
 #include <termios.h>
 #include <unistd.h>
 
+//macros to make code more readable
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-struct termios orig_termios;
+//data structure to store terminal attributes
+struct editorConfig{
+    struct termios orig_termios;
+};
+struct editorConfig E;
 
+//function to handle errors
 void die(const char *s){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
+//function to disable raw mode
 void disableRawMode(){
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1);
     die("tcsetattr");
 }
+//function to enable raw mode
 void enableRawMode(){
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -31,20 +43,51 @@ void enableRawMode(){
 
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+//function to read a keypress
+char editorReadKey(){
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1){
+        if(nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+//function to process keypresses
+void editorProcessKeypress(){
+    char c = editorReadKey();
+    switch(c){
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+    }
+}
+
+void editorDrawRows(){
+    int y;
+    for(y = 0; y < 24; y++){
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+//function to refresh the screen
+void editorRefreshScreen(){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+//main function
 int main() {
     enableRawMode();
 
     while(1){
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if(iscntrl(c)){
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if(c == CTRL_KEY('q')){
-            break;
-        }
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
 
     return 0;
